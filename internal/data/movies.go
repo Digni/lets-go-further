f03c905e022bfeadce6b3d621bf6a2d44f9c6ffb
Fabian Freimueller <fabian@freimueller.email>
2025-05-11 17:41:39 +0200

    wip

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -9,7 +9,7 @@ import (
 type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
-	Title     string    `json:"movie.`
+	Title     string    `json:"movie"`
 	Year      int32     `json:"year,omitzero"`
 	Runtime   int32     `json:"runtime,omitzero"`
 	Genres    []string  `json:"genres,omitzero"`
